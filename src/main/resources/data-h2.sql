8bacf0e1dc7e2543a21d51a1b7f0b0c40ca49363
Ronny <ronnyapinho@gmail.com>
2019-12-01 08:37:46 -0400

    Configurações do h2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,29 @@
-DELETE FROM PACIENTE;
+select * from PACIENTE;
 
-insert into PACIENTE  (ID_PACIENTE ,DATA_NACIMENTO ,ENDERECO ,NOME ,PROFISSAO ,TELEFONE ) values (1, '30/11/2019', 'Sem numero', 'Fulano de tal', 'Programador', '234324');
 
-insert into PACIENTE  (ID_PACIENTE ,DATA_NACIMENTO ,ENDERECO ,NOME ,PROFISSAO ,TELEFONE ) values (2, '30/11/2019', 'Sem numero', 'Ciclano', 'Programador', '234324');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (1,'6230-05', 'Adestrador de Animais Ocupação');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (2,'6231-05', 'Adestrador de Animais de trabalho (, Asininos e muares) Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (3,'2521-05', 'Administrador Ocupação');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (4,'1415-05', 'Administrador (serviço de hospedagem) Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (5,'2631-05', 'Administrador Apostólico Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (6,'1312-10', 'Administrador de Ambulatório Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (7,'2613-05', 'Administrador de Arquivos Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (8,'2123-05', 'Administrador de banco de dados Ocupação');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (9,'1415-05', 'Administrador de camping Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (10,'1417-15', 'Administrador de carteiras de câmbio e comércio exterior Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (11,'1417-20', 'Administrador de carteiras de crédito e cobrança Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (12,'1417-25', 'Administrador de carteiras de crédito imobiliário Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (13,'1424-05', 'Administrador de compras Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (14,'2522-10', 'Administrador de contadorias e registros fiscais Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (15,'5101-10', 'Administrador de edifícios Ocupação');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (16,'2521-05', 'Administrador de empresas Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (17,'1415-05', 'Administrador de empresas de hospedagem Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (18,'1411-20', 'Administrador de empresas de pesca Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (19,'2525-05', 'Administrador de fundos e carteiras de investimento Ocupação');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (20,'1311-15', 'Administrador de instituições culturais Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (21,'2521-05', 'Administrador de marketing Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (22,'1424-15', 'Administrador de materiais Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (23,'2521-05', 'Administrador de orçamento Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (24,'2521-05', 'Administrador de patrimônio Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (25,'2521-05', 'Administrador de pequena e média empresa Sinônimo');
+INSERT INTO PROFISSAO (IDPROFISSAO,CODIGO,DESCRICAO) VALUES (26,'1422-10', 'Administrador de pessoal Sinônimo');
